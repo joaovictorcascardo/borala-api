765637c9e5af2d1e763a50c2df422d986437616b
João Victor Pacheco <joaovictorpachecoleite@gmail.com>
2025-10-12 22:41:02 -0300

    feat(database): create events table

---
+++ src/database/migrations/20251013013804_create_events_table.ts
@@ -0,0 +1,21 @@
+import type { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+    return knex.schema.createTable('events', (table) => {
+        table.bigIncrements('id').primary();
+        table.string('name', 255).notNullable();
+        table.string('address', 255).notNullable();
+        table.decimal('latitude', 9, 6).notNullable();  
+        table.decimal('longitude', 9, 6).notNullable(); 
+        table.timestamp('starts_at').notNullable();
+        table.timestamp('ends_at').notNullable();
+        table.text('description').nullable();
+
+        table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
+        table.timestamp('updated_at').notNullable().defaultTo(knex.fn.now());
+    });
+}
+
+export async function down(knex: Knex): Promise<void> {
+    return knex.schema.dropTable('events');
+}
