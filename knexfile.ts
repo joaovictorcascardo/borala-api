ef07f34a378320912f4b8e42ca9860a7659adff7
João Victor Pacheco <joaovictorpachecoleite@gmail.com>
2025-10-12 22:13:07 -0300

    feat: initial project structure with users table migration

---
+++ knexfile.ts
@@ -0,0 +1,21 @@
+import type { Knex } from "knex";
+import path from 'path';
+
+const config: { [key: string]: Knex.Config } = {
+  development: {
+    client: "pg",
+    connection: {
+      host: '127.0.0.1',
+      port: 5432,
+      user: 'postgres',
+      password: '1209',
+      database: 'carona_api'
+    },
+    migrations: {
+      directory: path.join(__dirname, 'src', 'database', 'migrations'),
+      extension: 'ts' 
+    }
+  },
+};
+
+module.exports = config;
