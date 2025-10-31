1cd8eafc5988199e3f8fa19951904f52e24d85f0
elle4ry <elleary9@gmail.com>
2025-10-31 15:58:15 -0300

    refactor(bookings): update booking validator logic for improved data validation

--- src/validators/BookingValidator.ts
+++ src/validators/BookingValidator.ts
@@ -1,9 +1,11 @@
 import { z } from "zod";
 
 export const createBooking = z.object({
+  vehicle_id: z
+    .number({ message: "O ID do veículo deve ser um número." }),
   body: z.object({
     seats_booked: z
-      .number({ invalid_type_error: "O número de assentos deve ser um número." })
+      .number({ message: "O número de assentos deve ser um número." })
       .int({ message: "O número de assentos deve ser um inteiro." })
       .min(1, { message: "É necessário reservar pelo menos 1 assento." }),
   }),
@@ -11,8 +13,9 @@ export const createBooking = z.object({
 
 export const updateBooking = z.object({
   body: z.object({
-    status: z.enum(["CONFIRMED", "REJECTED", "CANCELLED"], {
-      errorMap: () => ({ message: "Status inválido." }),
-    }),
+    status: z
+      .enum(["CONFIRMED", "REJECTED", "CANCELLED"], {
+        message: "Status inválido.",
+      }),
   }),
 });
