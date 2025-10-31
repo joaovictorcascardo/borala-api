7c337e68a4b60c7cb4d7c51190c2c499188c69ac
elle4ry <elleary9@gmail.com>
2025-10-31 01:08:02 -0300

    feat(bookings): create booking DTO for request and response handling

--- src/validators/BookingValidator.ts
+++ src/validators/BookingValidator.ts
@@ -1,19 +1,18 @@
 import { z } from "zod";
 
 export const createBooking = z.object({
-    body: z.object({
-        seats_booked: z.number({
-            invalid_type_error: "O número d assentos deve ser um número.",
-        })
-        .int(("O número de asssentos "))
-        .min(1, "É necessario reservar pelo menos 1 assento"),
-    }),
+  body: z.object({
+    seats_booked: z
+      .number({ invalid_type_error: "O número de assentos deve ser um número." })
+      .int({ message: "O número de assentos deve ser um inteiro." })
+      .min(1, { message: "É necessário reservar pelo menos 1 assento." }),
+  }),
 });
 
 export const updateBooking = z.object({
-    body: z.object({
-        srarys: z.enum(["CONFIRMADO", "REJEITADO", "CANCELADO"], {
-            errorMap: () => ({ message: "status inválido"})
-        })
+  body: z.object({
+    status: z.enum(["CONFIRMED", "REJECTED", "CANCELLED"], {
+      errorMap: () => ({ message: "Status inválido." }),
     }),
+  }),
 });
