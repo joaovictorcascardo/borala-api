c5692cb90c1e27c76aa8e6a87e931d9c0e734091
Joao Prado <Joaovictorpachecoleite@gmail.com>
2025-11-08 20:09:03 -0300

    feat(data): add findById and updateAvailableSeats to Ride.Data

--- src/data/Ride.Data.ts
+++ src/data/Ride.Data.ts
@@ -1,17 +1,42 @@
 import { db } from "../database/connection";
 import { CreateRideDTO } from "../dto/RideDTO";
+import { Ride } from "../types/Ride";
+
 export class RideData {
-    async create(driverId: number, rideData: CreateRideDTO) {
-        try{
-            const dataToInsert = {
-                ...rideData,
-                driver_id: driverId,
-                status: "SCHEDULED",
-            };
-            const newRide = await db("rides").insert(dataToInsert).returning("*");
-            return newRide
-        }catch(error: any){
-            throw new Error(error.message);
-        }
+  async create(driverId: number, rideData: CreateRideDTO): Promise<Ride> {
+    try {
+      const dataToInsert = {
+        ...rideData,
+        driver_id: driverId,
+        status: "SCHEDULED",
+      };
+      const [newRide] = await db("rides").insert(dataToInsert).returning("*");
+      return newRide;
+    } catch (error: any) {
+      throw new Error(error.message);
     }
+  }
+
+  async findById(id: number): Promise<Ride | undefined> {
+    try {
+      const ride = await db("rides").where({ id }).first();
+      return ride;
+    } catch (error: any) {
+      throw new Error(error.message);
+    }
+  }
+  async updateAvailableSeats(id: number, newSeatCount: number): Promise<Ride> {
+    try {
+      const [updatedRide] = await db("rides")
+        .where({ id })
+        .update({
+          available_seats: newSeatCount,
+          updated_at: db.fn.now(),
+        })
+        .returning("*");
+      return updatedRide;
+    } catch (error: any) {
+      throw new Error(error.message);
+    }
+  }
 }
