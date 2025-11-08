3ec47cca891e826c13b4d30dff3d90475717ecef
Joao Prado <Joaovictorpachecoleite@gmail.com>
2025-11-08 19:03:40 -0300

    feat(app): register booking routes

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import { authRoutes } from "./routes/auth.Routes";
 import { vehicleRoutes } from "./routes/vehicle.routes";
 import { rideRoutes } from "./routes/ride.routes";
 import uploadConfig from "./upload-images/upload";
+import { bookingRoutes } from "./routes/booking.Routes";
 
 const app = express();
 app.use(express.json());
@@ -12,5 +13,6 @@ app.use("/authenticator", authRoutes);
 app.use("/users", userRoutes);
 app.use("/vehicles", vehicleRoutes);
 app.use("/rides", rideRoutes);
+app.use(bookingRoutes);
 
 export { app };
