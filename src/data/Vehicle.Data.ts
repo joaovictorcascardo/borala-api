05dc2d02fe019dae17327ba43af15575d66733d1
Joao Prado <Joaovictorpachecoleite@gmail.com>
2025-11-09 00:44:26 -0300

    refactor(data): move business logic from Vehicle.Data to Service layer

--- src/data/Vehicle.Data.ts
+++ src/data/Vehicle.Data.ts
@@ -1,76 +1,88 @@
 import { db } from "../database/connection";
 import { CreateVehicleDTO } from "../dto/VehicleDTO";
+import { Vehicle } from "../types/Vehicle";
 
+type UserVehicleListDTO = {
+  id: number;
+  brand: string;
+  model: string;
+  color: string;
+  license_plate: string;
+  year: number;
+  seats: number;
+};
 
-export class VehicleData{
-    async existingVehicle(license_plate: string): Promise<null>{
-        try{
-            const SearchPlate = await db("vehicles")
-                .where({ license_plate })
-                .first();
-            if (SearchPlate) {
-                throw new Error("Veículo com esta placa já cadastrado.");
-            }
-            return null;
-        }catch(error: any){
-            throw new Error(error.message);
-        }
+export class VehicleData {
+  async findByLicensePlate(
+    license_plate: string
+  ): Promise<Vehicle | undefined> {
+    try {
+      const vehicle = await db("vehicles").where({ license_plate }).first();
+      return vehicle;
+    } catch (error: any) {
+      throw new Error(error.message);
     }
-    async verifyVehicleByIdAndUserId(id: number, user_id: number){
-        try{
-            const vehicle = await db("vehicles")
-                .where({
-                    id,
-                    user_id,
-                })
-                .first();
-            if (!vehicle) {
-                throw new Error("Veículo não encontrado ou não pertence ao motorista.");
-            }
-            return null;
-        }catch(error:any){
-            throw new Error(error.message);
-        }
+  }
+  async findByIdAndUserId(
+    id: number,
+    user_id: number
+  ): Promise<Vehicle | undefined> {
+    try {
+      const vehicle = await db("vehicles")
+        .where({
+          id,
+          user_id,
+        })
+        .first();
+      return vehicle;
+    } catch (error: any) {
+      throw new Error(error.message);
     }
-    async createVehicle({ brand, model, color, license_plate, year, seats, userId }: CreateVehicleDTO):Promise<CreateVehicleDTO>{
-        try{
-            const [newVehicle] = await db("vehicles")
-                .insert({
-                    brand,
-                    model,
-                    color,
-                    license_plate,
-                    year,
-                    seats,
-                    user_id: userId,
-                })
-                .returning("*");
-            delete newVehicle.create_at;
-            delete newVehicle.update_at;
-            return newVehicle;
-        }catch(error: any){
-            throw new Error(error.message);
-        }
+  }
+
+  async createVehicle({
+    brand,
+    model,
+    color,
+    license_plate,
+    year,
+    seats,
+    userId,
+  }: CreateVehicleDTO): Promise<Vehicle> {
+    try {
+      const [newVehicle] = await db("vehicles")
+        .insert({
+          brand,
+          model,
+          color,
+          license_plate,
+          year,
+          seats,
+          user_id: userId,
+        })
+        .returning("*");
+
+      return newVehicle;
+    } catch (error: any) {
+      throw new Error(error.message);
     }
-    async FindByUserId(user_id: number): Promise<CreateVehicleDTO[]>{
-        try{
-            const UserVehicles = await db("vehicles")
-                .where({ user_id })
-                .select(
-                    "id",
-                    "brand",
-                    "model",
-                    "color",
-                    "license_plate",
-                    "year",
-                    "seats"
-                );
-            if(!UserVehicles){
-                throw new Error ("Nenhum veiculo vinculado ao Id informado.");
-            }
-            return UserVehicles;
-        }catch(error: any){
-            throw new Error(error.message);
-        }
+  }
+  async FindByUserId(user_id: number): Promise<UserVehicleListDTO[]> {
+    try {
+      const UserVehicles = await db("vehicles")
+        .where({ user_id })
+        .select(
+          "id",
+          "brand",
+          "model",
+          "color",
+          "license_plate",
+          "year",
+          "seats"
+        );
+      return UserVehicles;
+    } catch (error: any) {
+      throw new Error(error.message);
     }
+  }
 }
