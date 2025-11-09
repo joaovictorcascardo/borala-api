d281e245fb82c186c12ec93cc3e52965ab000d26
Joao Prado <Joaovictorpachecoleite@gmail.com>
2025-11-09 00:24:53 -0300

    refactor(types): define Vehicle database model

--- src/types/Vehicle.ts
+++ src/types/Vehicle.ts
@@ -1,9 +1,12 @@
-export interface CreateVehicleDTO {
-    brand: string;
-    model: string;
-    color: string;
-    license_plate: string;
-    year: number;
-    seats: number;
-    userId: number;
+export interface Vehicle {
+  id: number;
+  user_id: number;
+  brand: string;
+  model: string;
+  color: string;
+  license_plate: string;
+  year: number;
+  seats: number;
+  created_at: Date;
+  updated_at: Date;
 }
